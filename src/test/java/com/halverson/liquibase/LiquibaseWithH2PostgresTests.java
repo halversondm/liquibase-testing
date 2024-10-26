3ca3f6899c34a74b16b1115d18fdcdc74c7452dd
Daniel M. Halverson <daniel.m.halverson@gmail.com>
2024-10-25 21:00:10 -0500

    using test containers to work with Postgres and Oracle

---
+++ src/test/java/com/halverson/liquibase/LiquibaseWithH2PostgresTests.java
@@ -0,0 +1,34 @@
+package com.halverson.liquibase;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+
+@SpringBootTest
+@ActiveProfiles("h2-postgres")
+@Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class LiquibaseWithH2PostgresTests {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    @Order(1)
+    void runLiquibase() throws Exception {
+        log.info("Running Liquibase...");
+    }
+
+    @Test
+    @Order(2)
+    void testDataViaSchema() throws Exception {
+        CommonValidationAgainstDatabase.validateAgainstSchema(dataSource);
+    }
+}
